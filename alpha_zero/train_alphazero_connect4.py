317b81350c85f7077c26ead7ac815d5242619d71
Zhihan Yang <yangz2@carleton.edu>
2021-08-10 11:33:36 +0800

    minor fix

--- alpha_zero/train_alphazero_connect4.py
+++ alpha_zero/train_alphazero_connect4.py
@@ -10,9 +10,9 @@ from algo_components import PolicyValueNet, Buffer, generate_self_play_data, pla
 game_klass = Connect4
 num_games_for_training = 2000
 num_grad_steps = 5
-eval_freq = 50
+eval_freq = 1
 buffer_size = 10000
-batch_size = 512
+batch_size = 5
 num_mcts_iter_alphazero = 500
 num_mcts_iter_pure_mcts = 500
 
