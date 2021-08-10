59a5e0a94329ddf97b51b9877f2e358b03d846be
Zhihan Yang <yangz2@carleton.edu>
2021-08-10 10:11:16 +0800

    minor fix

--- alpha_zero/algo_components/__init__.py
+++ alpha_zero/algo_components/__init__.py
@@ -2,4 +2,5 @@ from algo_components.policy_value_net import PolicyValueNet
 from algo_components.buffer import Buffer
 from algo_components.node import Node
 from algo_components.mcts import mcts_one_iter
-
+from algo_components.self_play import generate_self_play_data
+from algo_components.evaluate import play_one_game_against_pure_mcts
--- alpha_zero/train_alphazero_connect4.py
+++ alpha_zero/train_alphazero_connect4.py
@@ -1,36 +1,83 @@
+import numpy as np
+import torch
+import torch.optim as optim
+
 from games import Connect4
-from algo_components import PolicyValueNet, Buffer
+from algo_components import PolicyValueNet, Buffer, generate_self_play_data, play_one_game_against_pure_mcts
 
+# @@@@@@@@@@ hyper-parameters @@@@@@@@@@
 
+game_klass = Connect4
 num_games_for_training = 2000
 num_grad_steps = 5
 eval_freq = 50
 buffer_size = 10000
 batch_size = 512
-num_mcts_iter = 500
+num_mcts_iter_alphazero = 500
+num_mcts_iter_pure_mcts = 500
 
-game = Connect4()
-policy_value_net = PolicyValueNet(game.board_width, game.board_height)
-buffer = Buffer(buffer_size, batch_size, geometric_augment=True)
+# @@@@@@@@@@ important objects @@@@@@@@@@
+
+board_width, board_height = game_klass().board.shape
+policy_value_net = PolicyValueNet(board_width, board_height)
+optimizer = optim.Adam(policy_value_net.parameters(), lr=1e-3, weight_decay=1e-4)  # l2 norm
+buffer = Buffer(board_width, board_height, buffer_size, batch_size)
+
+# @@@@@@@@@@ training loop @@@@@@@@@@
 
 for game_idx in range(num_games_for_training):
 
     states, mcts_probs, zs = generate_self_play_data(
-        game_klass=Connect4,
-        num_mcts_iter=num_mcts_iter,
-        guiding_policy=policy_value_net.policy_value_fn
+        game_klass=game_klass,
+        num_mcts_iter=num_mcts_iter_alphazero,
+        policy_value_fn=policy_value_net.policy_value_fn
     )
-    buffer.push(states, mcts_probs, winner)
+    buffer.push(states, mcts_probs, zs)
 
     if buffer.is_ready():
         for n in range(num_grad_steps):
             states_b, mcts_probs_b, zs_b = buffer.sample()
-            probs, value = nn(states_b)
+            predicted_probs, predicted_zs = policy_value_net(states_b)
 
-            # do we set probs for invalid moves to zero
-            # compute loss for (probs, mcts_probs) and (value, winner_z)
-            # use adam optimizer with weight decay to minimize the loss
+            loss_part1 = torch.mean((zs_b - predicted_zs) ** 2)
+            loss_part2 = torch.mean(torch.sum(mcts_probs_b * torch.log(predicted_probs), dim=1))
+            loss = loss_part1 + loss_part2
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
     if (game_idx + 1) % eval_freq:
-        # use low temperature here
-        play(alpha_zero, pure_mcts)
+
+        first_hand_scores = []
+        for i in range(10):
+            score = play_one_game_against_pure_mcts(
+                game_klass=game_klass,
+                num_mcts_iters_pure=num_mcts_iter_pure_mcts,
+                num_mcts_iters_alphazero=num_mcts_iter_alphazero,
+                policy_value_fn=policy_value_net.policy_value_fn,
+                first_hand="alphazero"
+            )
+            first_hand_scores.append(score)
+
+        second_hand_scores = []
+        for i in range(10):
+            score = play_one_game_against_pure_mcts(
+                game_klass=game_klass,
+                num_mcts_iters_pure=num_mcts_iter_pure_mcts,
+                num_mcts_iters_alphazero=num_mcts_iter_alphazero,
+                policy_value_fn=policy_value_net.policy_value_fn,
+                first_hand="pure_mcts"
+            )
+            second_hand_scores.append(score)
+
+        mean_first_hand_score = float(np.mean(first_hand_scores))
+        mean_second_hand_score = float(np.mean(second_hand_scores))
+
+        if mean_first_hand_score > 0.99:  # basically perfect
+            num_mcts_iter_pure_mcts += 500
+
+        print(f"@@@@@ Eval after {game_idx}/{num_games_for_training} "
+              f"games against pure-mcts {num_mcts_iter_pure_mcts} @@@@@")
+        print(f"First-hand score: {round(mean_first_hand_score, 2)}")
+        print(f"Second-hand score: {round(mean_second_hand_score, 2)}")
