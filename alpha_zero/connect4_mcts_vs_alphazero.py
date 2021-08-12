21e0ced52b8b0c3a8e7d40cc76334d6a2218becc
Zhihan Yang <yangz2@carleton.edu>
2021-08-13 07:25:32 +0800

    minor fix

---
+++ alpha_zero/connect4_mcts_vs_alphazero.py
@@ -0,0 +1,28 @@
+from algo_components import play_one_game_against_pure_mcts
+from games import Connect4
+from multiprocessing import Pool
+import torch
+from algo_components.policy_value_net import PolicyValueNet
+
+
+def func(first_hand):
+    policy_value_net = PolicyValueNet(*Connect4().board.shape)
+    policy_value_net.load_state_dict(torch.load("trained_models/pvnet_1200.pth", map_location=torch.device('cpu')))
+    outcome = play_one_game_against_pure_mcts(
+        Connect4,
+        num_mcts_iters_pure=5000,
+        num_mcts_iters_alphazero=500,
+        policy_value_fn=policy_value_net.policy_value_fn,
+        first_hand=first_hand,
+    )
+    return outcome
+
+
+if __name__ == '__main__':
+
+    num_games = 20
+
+    for first_hand in ["pure_mcts", "alphazero"]:
+        with Pool(num_games) as p:
+            outcomes = p.map(func, [first_hand] * num_games)
+        print(first_hand, outcomes)
