45c1f60087b7b835e5716376c955a11942f57358
Zhihan Yang <yangz2@carleton.edu>
2021-08-12 21:25:26 +0800

    minor fix

---
+++ alpha_zero/connect4_alphazero_vs_human.py
@@ -0,0 +1,39 @@
+from ast import literal_eval
+import time
+import copy
+
+# from games.tic_tac_toe import TicTacToe
+from games.connect4 import Connect4
+from algo_components.node import Node
+from algo_components.mcts import mcts_one_iter
+
+
+game = Connect4()
+
+while True:
+
+    print(game)
+
+    if game.current_player == -1:
+
+        root = Node(parent=None, prior_prob=1.0)
+
+        start = time.perf_counter()
+        for _ in range(10000):
+            mcts_one_iter(game, root)
+        end = time.perf_counter()
+
+        print('Decision time:', end - start)
+
+        move = root.get_move(temp=0)
+
+    else:
+
+        move = literal_eval(input("What's your move: "))
+        move = (move[0] - 1, move[1] - 1)
+
+    done, winner = game.evolve(move)
+    if done:
+        print(game)
+        print(f"Winner is player {winner}.")
+        break
