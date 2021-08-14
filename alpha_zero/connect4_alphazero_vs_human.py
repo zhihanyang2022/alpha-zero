2110357806f154ecee696a50c09efcdc440ca2f6
Zhihan Yang <yangz2@carleton.edu>
2021-08-14 21:52:03 +0800

    minor fix

--- alpha_zero/connect4_alphazero_vs_human.py
+++ alpha_zero/connect4_alphazero_vs_human.py
@@ -1,6 +1,7 @@
 from ast import literal_eval
 import time
 import torch
+from numpy import unravel_index
 
 # from games.tic_tac_toe import TicTacToe
 from games.connect4 import Connect4
@@ -12,29 +13,20 @@ from algo_components.policy_value_net import PolicyValueNet
 game = Connect4()
 
 policy_value_net = PolicyValueNet(*game.board.shape)
-policy_value_net.load_state_dict(torch.load("trained_models/pvnet_1200.pth", map_location=torch.device('cpu')))
+policy_value_net.load_state_dict(torch.load("trained_models/pvnet_2000.pth", map_location=torch.device('cpu')))
 
 print(game)
 
 while True:
 
-    if game.current_player == -1:
+    if game.current_player == 1:
 
-        pi_vec, val = policy_value_net.policy_value_fn(game.board * game.get_current_player(), game.get_valid_moves(), True)
+        pi_vec, _ = policy_value_net.policy_value_fn(game.board * game.get_current_player(), game.get_valid_moves(), True)
         pi_vec[pi_vec < 0.01] = 0
-        print(pi_vec.reshape(game.board.shape))
-        print(val)
+        
+        pi_vec_square = pi_vec.reshape(game.board.shape)
 
-        root = Node(parent=None, prior_prob=1.0)
-
-        start = time.perf_counter()
-        for _ in range(2):
-            mcts_one_iter(game, root, policy_value_fn=policy_value_net.policy_value_fn)
-        end = time.perf_counter()
-
-        print('Decision time:', end - start)
-
-        move = root.get_move(temp=0)
+        move = unravel_index(pi_vec_square.argmax(), pi_vec_square.shape)
 
     else:
 
@@ -42,9 +34,10 @@ while True:
         move = (move[0] - 1, move[1] - 1)
 
     done, winner = game.evolve(move)
-    # if game.get_previous_player() == 1:
-    #     print("Predicted score:", policy_value_net.policy_value_fn(game.board * game.get_previous_player(), game.get_valid_moves())[1])
     print(game)
+
+    if game.get_previous_player() == 1:
+        print("Predicted score:", -policy_value_net.policy_value_fn(game.board * game.get_current_player(), game.get_valid_moves())[1])
 
     if done:
         print(game)
