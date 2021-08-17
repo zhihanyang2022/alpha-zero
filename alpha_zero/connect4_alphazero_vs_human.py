bc76a3531821e94968129f310c255e6239e691fc
Zhihan Yang <yangz2@carleton.edu>
2021-08-17 10:56:15 +0800

    minor fix

--- alpha_zero/connect4_alphazero_vs_human.py
+++ alpha_zero/connect4_alphazero_vs_human.py
@@ -16,7 +16,7 @@ print(game)
 
 while True:
 
-    if game.current_player == -1:
+    if game.current_player == -1:  # alphazero is always first-hand
 
         pi_vec, _ = policy_value_net.policy_value_fn(game.board * game.get_current_player(), game.get_valid_moves(), True)
         pi_vec[pi_vec < 0.01] = 0
